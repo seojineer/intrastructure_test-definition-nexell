e2013a9abac4470955202ec87bd9271fee2ee292
seoji <seoji@nexell.co.kr>
2019-02-19 14:22:45 +0900

    update nexell-camera test yaml
    update nexell-camera test yaml
    
    Signed-off-by: seoji <seoji@nexell.co.kr>

--- automated/linux/nexell-camera/camera_test.sh
+++ automated/linux/nexell-camera/camera_test.sh
@@ -6,17 +6,17 @@ RESULT_FILE="${OUTPUT}/result.txt"
 echo "Camera Test[0] Start"
 dp_cam_test -m 0 -w 720 -h 480 -p 0 -f 0 -F 1 -t 0  -d 1 -c 100
 if [ $? -eq 0 ]; then
-	echo "camera-test-0 pass" > ${RESULT_FILE}
+	echo "camera-test-0 pass" >> ${RESULT_FILE}
 else
-	echo "camera-test-0 fail" > ${RESULT_FILE}
+	echo "camera-test-0 fail" >> ${RESULT_FILE}
 	exit 0
 fi
 
 echo "Camera Test[1] Start"
 dp_cam_test -m 1 -w 704 -h 480 -p 0 -f 0 -F 1 -t 2  -d 1 -c 100
 if [ $? -eq 0 ]; then
-	echo "camera-test-1 pass" > ${RESULT_FILE}
+	echo "camera-test-1 pass" >> ${RESULT_FILE}
 else
-	echo "camera-test-1 fail" > ${RESULT_FILE}
+	echo "camera-test-1 fail" >> ${RESULT_FILE}
 fi
 
