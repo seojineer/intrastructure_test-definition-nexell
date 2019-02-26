694b2ccd845822c7719cf41b7e4d57fd17a08c8d
seoji <seoji@nexell.co.kr>
2019-02-26 14:03:29 +0900

    modify nexell-sysbench test
    This patch adds nexell-sysbench script.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>

---
+++ automated/linux/sysbench/nexell-sysbench.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+logfile="/tmp/sysbench_log.txt"
+
+create_out_dir "${OUTPUT}"
+cd "${OUTPUT}"
+
+# TEST
+/usr/bin/sysbench --test=cpu --cpu-max-prime=10000 --num-threads=2 run | tee ${logfile}
+general_parser
+
+general_parser() {
+    ms=$(grep -m 1 "total time" "${logfile}" | awk '{print substr($NF,1,length($NF)-1)}')
+    add_metric "cpu-total-time" "pass" "${ms}" "s"
+}
