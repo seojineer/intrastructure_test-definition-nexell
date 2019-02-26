d4ee716986c18233357371e82cc87dc789dbd8fa
seoji <seoji@nexell.co.kr>
2019-02-26 14:34:53 +0900

    fix nexell-sysbench test
    fix nexell-sysbench test
    
    Signed-off-by: seoji <seoji@nexell.co.kr>

--- automated/linux/sysbench/nexell-sysbench.sh
+++ automated/linux/sysbench/nexell-sysbench.sh
@@ -2,6 +2,11 @@
 
 . ../../lib/sh-test-lib
 
+general_parser() {
+    ms=$(grep -m 1 "total time" "${logfile}" | awk '{print substr($NF,1,length($NF)-1)}')
+    add_metric "cpu-total-time" "pass" "${ms}" "s"
+}
+
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
@@ -14,7 +19,3 @@ cd "${OUTPUT}"
 /usr/bin/sysbench --test=cpu --cpu-max-prime=10000 --num-threads=2 run | tee ${logfile}
 general_parser
 
-general_parser() {
-    ms=$(grep -m 1 "total time" "${logfile}" | awk '{print substr($NF,1,length($NF)-1)}')
-    add_metric "cpu-total-time" "pass" "${ms}" "s"
-}
